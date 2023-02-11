865f39a7f2b91a7f763ac796632dafc84a5f79a4
Sujeet Sreenivasan <sujeetsr@gmail.com>
2023-02-11 09:31:39 -0800

    React example app

---
+++ src/components/Counter/counter.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+
+export const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className='card'>
+      <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
+      <p></p>
+    </div>
+  );
+};
